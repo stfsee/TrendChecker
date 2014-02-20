9fc2162aae0d0227a39dc60a64cd956a325e9443
unknown <stefan.seeba@gmx.de>
2014-02-20 16:06:17 +0100

    added title and LPKF to stocks

--- TrendCheck.groovy
+++ TrendCheck.groovy
@@ -549,6 +549,7 @@ public class TrendCheck {
 			trendCheck.check(stocks.get(i),days)
 		}
 		outputFile.write "<html><head>"
+		outputFile.write "<title>Trendcheck</title>"
 		outputFile.append "<link href=\"../../css/theme.blue.css\" rel=\"stylesheet\">"
 		outputFile.append "<script type=\"text/javascript\" src=\"../../lib/jquery-2.0.3.min.js\"></script>"
 		outputFile.append "<script type=\"text/javascript\" src=\"../../lib/jquery.tablesorter.min.js\"></script>"
